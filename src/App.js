088f4c9081523540ef017008fe6f538b11d7ff28
unknown <mareshi@appstec-me.com>
2022-04-29 11:10:46 +0530

    file added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import Home from './Components/Cart/Home';
-
+import Test from './sandeep/test'
 
 function App() {
   return (
     <div className="App">
      <Home/>
+     <Test/>
     </div>
   );
 }
